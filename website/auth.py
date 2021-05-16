02451e833c750e2ec53093795084f9b3d6eb0b02
Zsolt Palfi <zsolt.palfi@estudiantat.upc.edu>
2021-05-16 04:09:21 +0200

    4 commit

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask import Blueprint, render_template, request, flash, redirect, url_for 
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
@@ -57,7 +57,7 @@ def sign_up():
             new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
-            login_user(user, remember=True)
+            login_user(new_user, remember=True)
             flash('Account created!', category='succes')
             return redirect(url_for('views.home'))
 
